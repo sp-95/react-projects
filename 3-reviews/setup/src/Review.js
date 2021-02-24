cf7320539d47055f02cb1f7b2cdd69b895383b60
Shashanka Prajapati <shashankap95@gmail.com>
2021-02-24 13:26:12 +0545

    Refactor Review project

--- 3-reviews/setup/src/Review.js
+++ 3-reviews/setup/src/Review.js
@@ -4,36 +4,39 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
   const [index, setIndex] = useState(0);
-  const review = people[index];
+  const { name, job, image, text } = people[index];
   const total = people.length;
 
-  const handleNext = next => {
-    const newIndex = next ? 1 : -1;
-    setIndex(prev => (((prev + newIndex) % total) + total) % total);
-  }
+  const fixIndex = index => ((index % total) + total) % total;
+  const handlePrev = () => setIndex(prev => fixIndex(prev - 1));
+  const handleNext = () => setIndex(prev => fixIndex(prev + 1));
 
-  const handleSurprise = () => {
-    var newIndex;
-    do {
-      newIndex = Math.floor(Math.random() * total);
-    } while (newIndex === index);
+  const handleRandom = () => {
+    let newIndex = Math.floor(Math.random() * total);
+    newIndex = newIndex === index ? fixIndex(newIndex + 1) : newIndex;
     setIndex(newIndex);
   }
 
   return (
     <article className="review">
       <div className="img-container">
-        <img src={review.image} alt={review.name} className="person-img" />
-        <span className="quote-icon"><FaQuoteRight /></span>
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
       </div>
-      <h4 className="author">{review.name}</h4>
-      <p className="job">{review.job}</p>
-      <p className="info">{review.text}</p>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
       <div>
-        <button className="prev-btn" onClick={() => handleNext(false)}><FaChevronLeft /></button>
-        <button className="next-btn" onClick={() => handleNext(true)}><FaChevronRight /></button>
+        <button className="prev-btn" onClick={handlePrev}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={handleNext}>
+          <FaChevronRight />
+        </button>
       </div>
-      <button className="random-btn" onClick={handleSurprise}>surprise me</button>
+      <button className="random-btn" onClick={handleRandom}>surprise me</button>
     </article>
   );
 };
