e49c18a58fc61cd42d501ab1e361d31393d70998
Shashanka Prajapati <shashankap95@gmail.com>
2021-02-28 11:21:34 +0545

    Dynamic height for navbar based on number of links

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -5,18 +5,27 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
 
-  const handleNavToggle = () => setShowLinks(!showLinks)
+  useEffect(() => {
+    const linksContainerHeight = linksRef.current.getBoundingClientRect().height
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksContainerHeight}px`
+    } else {
+      linksContainerRef.current.style.height = `0px`
+    }
+  }, [showLinks])
 
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="coding addict" className="logo" />
-          <button className="nav-toggle" onClick={handleNavToggle}><FaBars /></button>
+          <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}><FaBars /></button>
         </div>
-        <div className={`links-container${showLinks ? " show-container" : ""}`}>
-          <ul className="links">
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map(({ id, url, text }) => (
               <li key={id}><a href={url}>{text}</a></li>
             ))}
