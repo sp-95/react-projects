fb3cb2c57ba65eabc0056a9caf0d97bdf42de732
Shashanka Prajapati <shashankap95@gmail.com>
2021-02-28 12:08:58 +0545

    Display sidebar

--- 12-sidebar-modal/setup/src/App.js
+++ 12-sidebar-modal/setup/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <main>
       <Home />
+      <Sidebar />
     </main>
   )
 }
--- 12-sidebar-modal/setup/src/Sidebar.js
+++ 12-sidebar-modal/setup/src/Sidebar.js
@@ -4,7 +4,28 @@ import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
 
 const Sidebar = () => {
-  return <h2>sidebar</h2>
+  return (
+    <nav className="sidebar show-sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" className="logo" />
+        <button className="close-btn"><FaTimes /></button>
+      </div>
+      <ul className="links">
+        {links.map(({ id, url, text, icon }) => (
+          <li key={id}>
+            <a href={url}>{icon}{text}</a>
+          </li>
+        ))}
+      </ul>
+      <ul className="social-icons">
+        {social.map(({ id, url, icon }) => (
+          <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
 }
 
 export default Sidebar
