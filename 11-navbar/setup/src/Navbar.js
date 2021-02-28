9bd5b336104946f5b0e9af2824394b18192d7950
Shashanka Prajapati <shashankap95@gmail.com>
2021-02-28 10:16:38 +0545

    Navbar initial setup

--- 11-navbar/setup/src/Navbar.js
+++ 11-navbar/setup/src/Navbar.js
@@ -1,10 +1,29 @@
 import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
+import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
-  return <h4>navbar</h4>
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="coding addict" className="logo" />
+          <div className="nav-toggle"><FaBars /></div>
+        </div>
+        <ul className="links">
+          {links.map(({ id, url, text }) => (
+            <li key={id}><a href={url}>{text}</a></li>
+          ))}
+        </ul>
+        <ul className="social-icons">
+          {social.map(({ id, url, icon }) => (
+            <li key={id}><a href={url}>{icon}</a></li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
