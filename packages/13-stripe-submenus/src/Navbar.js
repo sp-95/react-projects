df868c75a833c2c40ad8c49bff009c9a08d1da92
Shashanka Prajapati <shashankap95@gmail.com>
2021-03-06 09:17:28 +0545

    Initial setup for Stripe submenus

--- packages/13-stripe-submenus/src/Navbar.js
+++ packages/13-stripe-submenus/src/Navbar.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
+import { useAppContext } from './context'
 
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  const { sublinks } = useAppContext()
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stripe" />
+          <button className="btn toggle-btn">
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          {sublinks.map(({ page }, index) => (
+            <li key={index}>
+              <button className="link-btn">{page}</button>
+            </li>
+          ))}
+        </ul>
+        <button className="btn signin-btn">sign in</button>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
--- packages/13-stripe-submenus/src/context.js
+++ packages/13-stripe-submenus/src/context.js
@@ -1,2 +1,16 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext()
+
+const AppProvider = ({children}) => {
+    return <AppContext.Provider value={{
+        sublinks
+    }}>
+        {children}
+    </AppContext.Provider>
+}
+
+export const useAppContext = () => useContext(AppContext)
+
+export default AppProvider
