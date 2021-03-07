a11a2157292148def2597bb634d6cddd8eb26654
Shashanka Prajapati <shashankap95@gmail.com>
2021-03-08 00:37:49 +0545

    Create the about page

--- packages/15-cocktails/src/pages/About.js
+++ packages/15-cocktails/src/pages/About.js
@@ -2,9 +2,16 @@ import React from "react"
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti nihil
+        veniam nobis ea, sapiente ipsa iste eos fugit explicabo dignissimos?
+        Quae et sunt dolor voluptates? Quaerat possimus hic illo cum sequi optio
+        a dolorum. Id quas optio cupiditate tempore possimus magni placeat,
+        cumque enim quos fugiat praesentium vel aliquam sit!
+      </p>
+    </section>
   )
 }
 
